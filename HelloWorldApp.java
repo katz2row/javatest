adbb50d9597535dc357fd80cec0a97a27746a8f5
Charity K <katz2row@users.noreply.github.com>
2018-05-14 22:38:00 -0700

    Added Loop - Hello World Three Times

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -4,6 +4,9 @@
  */
 class HelloWorldApp {
     public static void main(String[] args) {
+        int i;
+		for (i = 0; i < 3; i++) {
         System.out.println("Hello World!"); // Display the string.
+        }
     }
 }
