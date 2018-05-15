8d8f3307b625d3dddc303ca678c2dc4de1919f9e
Charity Katz <katz2row@gmail.com>
2018-05-14 23:06:41 -0700

    Added question and conditional

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,11 +2,24 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
+import java.util.Scanner;
 class HelloWorldApp {
     public static void main(String[] args) {
-        int i;
-		for (i = 0; i < 3; i++) {
-        System.out.println("Hello World!"); // Display the string.
-        }
+		
+		int i;
+		
+		Scanner reader = new Scanner(System.in);
+		
+		String ask_how;
+		System.out.print("Do you want to be polite or nag? Enter your answer (Polite or Nag): ");
+		ask_how = reader.next();
+		
+		if (ask_how.equals("Nag"))
+			for (i = 0; i < 3; i++) {
+				System.out.println("Hello World!"); // Display the string.
+				}
+		
+		else 
+			System.out.println("Hello World!"); // Display the string.
     }
 }
